48625335c9d1113e05bf1c1441c9fdb4e7ea3e71
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-11-07 11:39:36 +0000

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 
-gulp.task('build', shell.task([
-        'cp -fR _book/.git/ _book-git',
-        'gitbook build', 
-        'mv _book-git _book/.git',
-        'cp -fR post-book/* _book/'
+gulp.task('default', ['deploy']);
+
+gulp.task('build', shell.task([          // gitbook destroys everything in the _book directory!
+        'cp -fR _book/.git/ _book-git', // save git info
+        'gitbook build',                // build HTML in _book
+        'mv _book-git _book/.git',      // restore git info
+        'cp -fR post-book/* _book/'     // restore README.md in _book (and other aditional files)
       ],
       { verbose: true }
   )
 );
 
 gulp.task('deploy', ['build'], shell.task(
-    [ 'cd _book && git ci -am && git push github master']
+    [ 'cd _book && git ci -am "new version" && git push git@github.com:ULL-PFPDI-GITHUB-1718/ull-pfpdi-github-1718.github.io.git master']
   )
 );
+
+gulp.task('ghrepo', shell.task(
+  'hub browse ULL-PFPDI-GITHUB-1718/ull-pfpdi-github-1718.github.io'
+));
+
+gulp.task('apuntes', shell.task(
+  'open https://ull-pfpdi-github-1718.github.io'
+));
+
+gulp.task("pre-install", shell.task([
+      "npm i -g gitbook-cli",
+]));
